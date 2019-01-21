41720e5d4d480bf77f8cb09e56ac209825c4b97e
Tom Brow <tom@tombrow.com>
2019-01-21 10:56:38 -0800

    show real task names in UI

--- TimelordIntentsUI/IntentViewController.swift
+++ TimelordIntentsUI/IntentViewController.swift
@@ -11,21 +11,16 @@ class IntentViewController: UITableViewController, INUIHostedViewControlling {
         context: INUIHostedViewContext,
         completion: @escaping (Bool, Set<INParameter>, CGSize) -> Void)
     {
-        // As the docs say, "SiriKit calls this method method once with zero
-        // parameters… If there are still parameters to be displayed,
-        // SiriKit calls this method one or more additional times with a set
-        // of INParameter objects."
-        //
-        // I don't understand why SiriKit does that, but if we return true
-        // multiple times, the resulting UI contains multiple
-        // IntentViewControllers stacked vertically. We only want one, so
-        // we must return false in the empty parameters case.
-        let success = !parameters.isEmpty
+        reminders = parameters
+            .compactMap(interaction.parameterValue(for:))
+            .compactMap { $0 as? INTask }
+            .compactMap(Reminder.init(task:))
+        tableView.reloadData()
         
         let configuredParameters = parameters
         let desiredSize = tableView.contentSize
         completion(
-            success,
+            !reminders.isEmpty,
             configuredParameters,
             desiredSize)
     }
@@ -41,7 +36,7 @@ class IntentViewController: UITableViewController, INUIHostedViewControlling {
         numberOfRowsInSection section: Int)
         -> Int
     {
-        return 1
+        return reminders.count
     }
     
     override func tableView(
@@ -49,6 +44,7 @@ class IntentViewController: UITableViewController, INUIHostedViewControlling {
         cellForRowAt indexPath: IndexPath)
         -> UITableViewCell
     {
+        let reminder = reminders[indexPath.row]
         let cellIdentifier = "Cell"
         let cell = tableView
             .dequeueReusableCell(
@@ -56,8 +52,30 @@ class IntentViewController: UITableViewController, INUIHostedViewControlling {
             ?? UITableViewCell(
                 style: .value1,
                 reuseIdentifier: cellIdentifier)
-        cell.textLabel?.text = "Take eggplant out"
+        cell.textLabel?.text = reminder.name
         cell.detailTextLabel?.text = "20:00"
         return cell
+    }
+    
+    // MARK: private
+    
+    private var reminders: [Reminder] = []
+}
+
+private struct Reminder {
+    let name: String
+    let date: Date
+    
+    init?(task: INTask) {
+        guard
+            let startDateComponents = task
+                .temporalEventTrigger?
+                .dateComponentsRange
+                .startDateComponents,
+            let startDate = Calendar.current.date(
+                from: startDateComponents)
+            else { return nil }
+        name = task.title.spokenPhrase
+        date = startDate
     }
 }
