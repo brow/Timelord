c7b647e5cfec748c5daa11fc8f9c6366d935aa34
Tom Brow <tom@tombrow.com>
2019-01-24 13:18:51 -0800

    don't use storyboard

--- Timelord/ViewController.swift
+++ Timelord/ViewController.swift
@@ -1,20 +1,9 @@
-//
-//  ViewController.swift
-//  Timelord
-//
-//  Created by Tom Brow on 1/20/19.
-//  Copyright © 2019 Tom Brow. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    override func loadView() {
+        super.loadView()
+        
+        view.backgroundColor = .white
     }
-
-
 }
-
