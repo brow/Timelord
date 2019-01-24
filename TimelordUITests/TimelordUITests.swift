f6c8709872e059e59f517e3e386bcb5f849ca84e
Tom Brow <tom@tombrow.com>
2019-01-24 12:20:32 -0800

    refactor

--- TimelordUITests/TimelordUITests.swift
+++ TimelordUITests/TimelordUITests.swift
@@ -1,22 +1,29 @@
 import XCTest
 
+// UI testing Siri:
+// https://stackoverflow.com/a/45740212/293215
+
 class TimelordUITests: XCTestCase {
 
     func testExample() {
-        // UI testing Siri:
-        // https://stackoverflow.com/a/45740212/293215
         let siri = XCUIDevice.shared.siriService
         siri.activate(
             voiceRecognitionText: "In Timelord remind me in 30 minutes to take yams out")
-        wait(
-            for: [
-                expectation(
-                    for: NSPredicate { _, _ in sleep(3); return false },
-                    evaluatedWith: siri,
-                    handler: nil)],
-            timeout: 5)
+        wait(seconds: 3)
         siri.activate(
             voiceRecognitionText: "Show my Timelord reminders")
     }
 
+    private func wait(seconds: Int) {
+        wait(
+            for: [
+                expectation(
+                    for: NSPredicate { _, _ in
+                        sleep(UInt32(seconds))
+                        return true
+                    },
+                    evaluatedWith: self,
+                    handler: nil)],
+            timeout: TimeInterval(seconds) + 1)
+    }
 }
