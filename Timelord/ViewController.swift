a723c8e104692f2fdc06915d031808aec91db2b4
Tom Brow <tom@tombrow.com>
2019-01-31 12:55:39 -0600

    button

--- Timelord/ViewController.swift
+++ Timelord/ViewController.swift
@@ -5,5 +5,31 @@ class ViewController: UIViewController {
         super.loadView()
         
         view.backgroundColor = .white
+        
+        let button = UIButton(type: .system)
+        button.setTitle(
+            "Allow Notifications",
+            for: .normal)
+        
+        let stackView = UIStackView(
+            arrangedSubviews: [button])
+        stackView.axis = .vertical
+        stackView.alignment = .leading
+        view.addSubview(stackView)
+        
+        // Layout
+        
+        let marginsGuide = view.layoutMarginsGuide
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.topAnchor
+            .constraint(equalTo: marginsGuide.topAnchor)
+            .isActive = true
+        stackView.leadingAnchor
+            .constraint(equalTo: marginsGuide.leadingAnchor)
+            .isActive = true
+        stackView.trailingAnchor
+            .constraint(lessThanOrEqualTo: marginsGuide.trailingAnchor)
+            .isActive = true
     }
 }
